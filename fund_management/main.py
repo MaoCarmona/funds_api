dc64d5dcead3535030a48ca1c8d8a3e5c77ea720
MaoCarmona <89806416+MaoCarmona@users.noreply.github.com>
2024-09-15 21:57:24 -0500

    Add Cors

--- fund_management/main.py
+++ fund_management/main.py
@@ -1,7 +1,8 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fund_management.infrastructure.database import MongoDB
-from fund_management.api.routes import fund_routes
+from fund_management.api.routes import fund_routes, transaction_routes
 from fund_management.infrastructure.environment import config
 
 @asynccontextmanager
@@ -16,9 +17,17 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
+
 @app.get("/health")
 def health():
-    return {"status": 200, "msg": "Api Ok" }
-    
-app.include_router(fund_routes.router, prefix="/api")
+    return {"status": 200, "msg": "Api Ok"}
 
+app.include_router(fund_routes.router, prefix="/api")
+app.include_router(transaction_routes.router, prefix="/api")
